15e9ed2a03cb2a76dc0388d43e1bea2b88b1fe0b
Zhuang Ma <ChumpMa@gmail.com>
2015-11-06 14:36:30 +0800

    tab --> spaces

--- timew/timew/timew.cpp
+++ timew/timew/timew.cpp
@@ -6,48 +6,48 @@
 #include <Windows.h>
 
 void usage() {
-	_tprintf(_T("time command for windows:\n"));
-	_tprintf(_T("  timew.exe <yourprogram>\n"));
-	_tprintf(_T("\nsuggestions and bug report: https://github.com/mzlogin/time-for-windows\n"));
+    _tprintf(_T("time command for windows:\n"));
+    _tprintf(_T("  timew.exe <yourprogram>\n"));
+    _tprintf(_T("\nsuggestions and bug report: https://github.com/mzlogin/time-for-windows\n"));
 }
 
 int _tmain(int argc, _TCHAR* argv[]) {
 
-	if (argc != 2) {
-		usage();
-		return 0;
-	} else {
-		if (-1 == _taccess(argv[1], 0)) {
-			_tprintf(_T("%s cannot found!\n"), argv[1]);
-		}
+    if (argc != 2) {
+        usage();
+        return 0;
+    } else {
+        if (-1 == _taccess(argv[1], 0)) {
+            _tprintf(_T("%s cannot found!\n"), argv[1]);
+        }
 
-		STARTUPINFO si;
-		PROCESS_INFORMATION pi;
-		ZeroMemory(&si, sizeof(si));
-		si.cb = sizeof(si);
-		si.dwFlags = STARTF_USESHOWWINDOW;
+        STARTUPINFO si;
+        PROCESS_INFORMATION pi;
+        ZeroMemory(&si, sizeof(si));
+        si.cb = sizeof(si);
+        si.dwFlags = STARTF_USESHOWWINDOW;
 
-		ZeroMemory(&pi, sizeof(pi));
+        ZeroMemory(&pi, sizeof(pi));
 
-		BOOL bRet = FALSE;
-		bRet = CreateProcess(argv[1], NULL, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi);
+        BOOL bRet = FALSE;
+        bRet = CreateProcess(argv[1], NULL, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi);
 
-		if (bRet) {
-			WaitForSingleObject(pi.hProcess, INFINITE);
+        if (bRet) {
+            WaitForSingleObject(pi.hProcess, INFINITE);
 
-			FILETIME createTime, exitTime, kernelTime, userTime;
-			bRet = GetProcessTimes(pi.hProcess, &createTime, &exitTime, &kernelTime, &userTime);
+            FILETIME createTime, exitTime, kernelTime, userTime;
+            bRet = GetProcessTimes(pi.hProcess, &createTime, &exitTime, &kernelTime, &userTime);
 
-			if (bRet) {
-				_tprintf(_T("\n%s finished\n"), argv[1]);
-				_tprintf(_T("cost time: %dms\n"), (exitTime.dwLowDateTime - createTime.dwLowDateTime) / 10000);
-			}
+            if (bRet) {
+                _tprintf(_T("\n%s finished\n"), argv[1]);
+                _tprintf(_T("cost time: %dms\n"), (exitTime.dwLowDateTime - createTime.dwLowDateTime) / 10000);
+            }
 
-			CloseHandle(pi.hThread);
-			CloseHandle(pi.hProcess);
-		}
-	}
+            CloseHandle(pi.hThread);
+            CloseHandle(pi.hProcess);
+        }
+    }
 
-	return 0;
+    return 0;
 }
 
